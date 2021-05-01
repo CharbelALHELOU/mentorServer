8dd5cf18f15c13e10c5da1198cf44d5823f42f76
ging <charbel.alhelou@ensa-paris.fr>
2021-05-01 22:38:01 +0200

    ...

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
