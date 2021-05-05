64d9d55f83a28c3535ddc42cda175b0cbd7c0803
ging <charbel.alhelou@ensa-paris.fr>
2021-05-05 02:16:07 +0200

    ...

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/resume", express.static(path.join(__dirname, "resume")));
 
 app.use("/user", userRoutes);
 app.use("/shop", shopRoutes);
-
 
 
 mongoose
