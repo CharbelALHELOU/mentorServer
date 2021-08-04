5745e979694f423446f83054a74fee90824fbb0c
ging <charbel.alhelou@ensa-paris.fr>
2021-08-04 22:17:00 +0200

    ...

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
