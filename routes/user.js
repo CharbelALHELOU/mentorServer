f9e3435ca7f5e88245c65b686cf69e7c7729278a
ging <charbel.alhelou@ensa-paris.fr>
2021-08-05 13:28:51 +0200

    ...

--- routes/user.js
+++ routes/user.js
@@ -116,7 +116,7 @@ router.post(
           from: "mentorpack.contact@gmail.com", // sender address
           to: newUser.email , // list of receivers
           subject: "Welcome to MentorPack", // Subject line
-          html: '<h2>Welcome '+ newUser.name +'</h2><p>You are successfully registered !</p> <p>Please find below the link to our secured platform to submit your resume.</p>'// plain text body
+          html: '<h2>Welcome '+ newUser.name +' ! </h2><p>You are successfully registered !</p> <p>Please find below the link to our secured platform to submit your resume.</p>'// plain text body
         }).then(info => {
           console.log({ info });
         }).catch(console.error);
