dd31a2567558868493b272f0f889aeb5827c0655
ging <charbel.alhelou@ensa-paris.fr>
2021-08-08 11:06:20 +0200

    ...

--- routes/user.js
+++ routes/user.js
@@ -76,7 +76,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'mentorpack.contact@gmail.com',
+    user: 'contact@mentor-pack.com',
     pass: 'zsepvcdpogakytry',
   },
 });
@@ -109,7 +109,7 @@ router.post(
         });
         console.log("user created .... sending email ....");
         transporter.sendMail({
-          from: "mentorpack.contact@gmail.com", // sender address
+          from: "contact@mentor-pack.com", // sender address
           to: newUser.email, // list of receivers
           subject: "Welcome to MentorPack", // Subject line
           html: '<h2>Welcome ' + newUser.name + ' ! </h2><p>You are successfully registered !</p>' +
@@ -277,7 +277,7 @@ router.post(
         const updatedUser = await oldUser.save();
 
         transporter.sendMail({
-          from: "mentorpack.contact@gmail.com", // sender address
+          from: "contact@mentor-pack.com", // sender address
           to: oldUser.email, // list of receivers
           subject: "Resume well recieved", // Subject line
           html: '<h2>Hello ' + oldUser.name.split(" ")[0] + ' ! </h2><p>You are successfully registered !</p> <p>We have successfully recieved your resume and we will start looking at your profile to find you the best match</p>'// plain text body
@@ -313,7 +313,7 @@ router.post(
       const nameUser = oldUser.name.split(" ");
       
       transporter.sendMail({
-        from: "mentorpack.contact@gmail.com", // sender address
+        from: "contact@mentor-pack.com", // sender address
         to: mentor.email, // list of receivers
         subject: "You have a new Mentee", // Subject line
         html: '<h2>Hello ' + nameMentor[0] + ' ! </h2><p> You have been assigned a new Mentee.</p>'+
@@ -333,172 +333,17 @@ router.post(
 );
 
 
-
-/*
-router.post('/upload', function (req, res) {
-  console.log(req);
-  var form = new formidable.IncomingForm();
-  form.multiples = true; //enable mutiple for formidable
-  form.uploadDir = "resume/";
-  var file_name, file_ext;
-  var file_id;
-
-  form.parse(req, function (err, fields, files) {
-    // if upload one file
-    if (!Array.isArray(files.file)) {
-      // `file` is the name of the <input> field of type `file`
-      oldpath = files.file.path;
-      newpath = form.uploadDir + files.file.name;
-      file_name = files.file.name;
-      file_ext = files.file.type;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-      });
-    }
-    //end if    ///-------------------------------------------------------------------------
-    ///
-    // Load client secrets from a local file.
-    fs.readFile('./routes/credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Drive API.
-      authorize(JSON.parse(content), uploadFile);//------
-    });
-    /////
-    // Target forlder for the Uploaded file.
-    const targetFolderId = "1LZkwgaHJqkD5J2A5rTz8B_bH9mX2QnjZ";
-    function uploadFile(auth) {
-      const drive = google.drive({ version: 'v3', auth });
-
-      //upload one file
-      var fileMetadata = {
-        'name': file_name,
-        parents: [targetFolderId]
-      };
-      var media = {
-        mimeType: file_ext,
-        body: fs.createReadStream(path.join(__dirname, 'resume/', file_name))
-      };
-      drive.files.create({
-
-        resource: fileMetadata,
-        media: media,
-        fields: 'id'
-      }, function (err, file) {
-        if (err) {
-          // Handle error
-          console.error(err);
-        } else {
-          file_id = file.data.id;
-          console.log(`file Id:${file.data.id}`);
-        }
-      });
-    }
-  })
-});
-
-
-
-
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-/*
-
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-
-
-
-
-
-router.post(
-  "/upload/:id",
-  multer({ storage: storage }).single("resumeUrl"),
-  (req, res) => {
-    var FileId = "";
-    const url = req.protocol + "://" + req.get("host");
-    User.findOne({ id: req.params.id }).then((user) => {
-      if (user) {
-        errors.id = "Invalid User";
-        return res.status(400).json({ success: false, message: errors.id });
-      } else {
-
-        fs.readFile('./routes/credentials.json', (err, content) => {
-          if (err) return console.log('Error loading client secret file:', err);
-          // Authorize a client with credentials, then call the Google Drive API.
-          authorize(JSON.parse(content), uploadFile);//------
-        });
-      }
-    });
-
-
-
-    const targetFolderId = "1dHuEXWVSnyc2ljhtyDGW9Tbm8IyJ0wh6";
-    function uploadFile(auth) {
-      const drive = google.drive({ version: 'v3', auth });
-      //upload one file
-      var fileMetadata = {
-        'name': req.file.filename,
-        parents: [targetFolderId]
-      };
-      var media = {
-        mimeType: req.file.mimetype,
-        body: fs.createReadStream(path.join('resume/', req.file.filename))
-      };
-      drive.files.create({
-
-        resource: fileMetadata,
-        media: media,
-        fields: 'id'
-      }, function (err, file) {
-        if (err) {
-          // Handle error
-          res.status(400).json({
-            success: false,
-            message: err,
-          })
-        } else {
-          file_id = file.data.id;
-          FileId = file.data.id;
-        }
-      });
-    }
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const removeMentor = await mentor.remove();
+    res.json({ success: true, message: name + " was deleted" });
+  } catch {
+    res
+      .status(404)
+      .json({ success: false, message: "Failed to delete mentor" });
   }
-);
-
-*/
+});
 
 
 
