6f68948833bb96877872326cb478d97cc5d04734
ging <charbel.alhelou@ensa-paris.fr>
2021-08-23 12:41:30 +0200

    ...

--- routes/shop.js
+++ routes/shop.js
@@ -14,41 +14,41 @@ const validateMentorInput = require("../validation/mentor"); // mentor validatio
  */
 // Multer configuration
 const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
 };
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValidFile = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValidFile) error = null;
-    cb(error, "images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-")
-      .split(".")
-      .slice(0, -1)
-      .join(".");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
+    destination: (req, file, cb) => {
+        const isValidFile = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error("Invalid mime type");
+        if (isValidFile) error = null;
+        cb(error, "images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname
+            .toLowerCase()
+            .split(" ")
+            .join("-")
+            .split(".")
+            .slice(0, -1)
+            .join(".");
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + "-" + Date.now() + "." + ext);
+    },
 });
 
 //--------------------------------Middleware--------------------------------//
 
 function verifyToken(req, res, next) {
-  if (!req.headers.authorization)
-    return res.status(401).send("Unauthorized request");
-  let token = req.headers.authorization.split(" ")[1];
-  if (token === "null") return res.status(401).send("Unauthorized request");
-  let payload = jwt.verify(token, secretOrKey);
-  if (!payload) return res.status(401).send("Unauthorized request");
-  req.userId = payload.id;
-  next();
+    if (!req.headers.authorization)
+        return res.status(401).send("Unauthorized request");
+    let token = req.headers.authorization.split(" ")[1];
+    if (token === "null") return res.status(401).send("Unauthorized request");
+    let payload = jwt.verify(token, secretOrKey);
+    if (!payload) return res.status(401).send("Unauthorized request");
+    req.userId = payload.id;
+    next();
 }
 
 //----------------------------------Routes----------------------------------//
@@ -58,72 +58,73 @@ function verifyToken(req, res, next) {
 // @desc    Get mentors
 // @access  Private
 router.get("/mentors", (req, res) => {
-  Mentor.find()
-    .sort({ updatedAt: -1 })
-    .then((mentors) => res.json({ success: true, mentors }))
-    .catch((err) =>
-      res.status(404).json({ success: false, message: "No mentors found" })
-    );
+    Mentor.find()
+        .sort({ updatedAt: -1 })
+        .then((mentors) => res.json({ success: true, mentors }))
+        .catch((err) =>
+            res.status(404).json({ success: false, message: "No mentors found" })
+        );
 });
 
 // @route   POST /shop
 // @desc    Create mentor
 // @access  Private
 router.post(
-  "/",
-  multer({ storage: storage }).single("imageUrl"),
-  (req, res) => {
-    const { errors, isValid } = validateMentorInput(req.body);
-    if (!isValid) return res.status(400).json({ success: false, errors });
-    // Constructing a url to the server
-    const url = "https://thawing-journey-90753.herokuapp.com";
+    "/",
+    multer({ storage: storage }).single("imageUrl"),
+    (req, res) => {
+        const { errors, isValid } = validateMentorInput(req.body);
+        if (!isValid) return res.status(400).json({ success: false, errors });
+        // Constructing a url to the server
+        const url = "https://thawing-journey-90753.herokuapp.com";
 
-    const newMentor = new Mentor({
-      name: req.body.name,
-      university: req.body.university,
-      position: req.body.position,
-      description: req.body.description,
-      linkedinUrl: req.body.linkedinUrl,
-      imageUrl: url + "/images/" + req.file.filename,
-    });
-    newMentor.save()
-      .then((mentor) => res.json({ success: true, mentor }))
-      .catch((err) =>
-        res.status(404).json({
-          success: false,
-          message: err,
-        })
-      );
+        const newMentor = new Mentor({
+            name: req.body.name,
+            university: req.body.university,
+            position: req.body.position,
+            description: req.body.description,
+            linkedinUrl: req.body.linkedinUrl,
+            email: req.body.email,
+            imageUrl: url + "/images/" + req.file.filename,
+        });
+        newMentor.save()
+            .then((mentor) => res.json({ success: true, mentor }))
+            .catch((err) =>
+                res.status(404).json({
+                    success: false,
+                    message: err,
+                })
+            );
 
-  }
+    }
 );
 
 
 // @route   DELETE /shop/:id
 // @desc    Delete mentor
 // @access  Private
-router.delete("/:id", verifyToken, async (req, res) => {
-  try {
-    const mentor = await Mentor.findById(req.params.id);
-    let name = mentor.name;
-    // deleting image file
-    if (mentor) {
-      let url = req.protocol + "://" + req.get("host");
-      let Path = mentor.imageUrl.split(url).pop();
-      fs.unlink("." + Path, (err) => {
-        if (err)
-          return res
-            .status(400)
-            .json({ success: false, message: "Failed to delete image file" });
-      });
+router.delete("/:id", verifyToken, async(req, res) => {
+    try {
+        const mentor = await Mentor.findById(req.params.id);
+        let name = mentor.name;
+        // deleting image file
+        if (mentor) {
+            let url = req.protocol + "://" + req.get("host");
+            let Path = mentor.imageUrl.split(url).pop();
+            fs.unlink("." + Path, (err) => {
+                if (err)
+                    return res
+                        .status(400)
+                        .json({ success: false, message: "Failed to delete image file" });
+            });
+        }
+        const removeMentor = await mentor.remove();
+        res.json({ success: true, message: name + " was deleted" });
+    } catch {
+        res
+            .status(404)
+            .json({ success: false, message: "Failed to delete mentor" });
     }
-    const removeMentor = await mentor.remove();
-    res.json({ success: true, message: name + " was deleted" });
-  } catch {
-    res
-      .status(404)
-      .json({ success: false, message: "Failed to delete mentor" });
-  }
 });
 
 
@@ -154,24 +155,24 @@ router.post(
 
 
 router.put(
-  "/show/:id", verifyToken,
-  async (req, res) => {
-    try {
+    "/show/:id", verifyToken,
+    async(req, res) => {
+        try {
 
-      const oldMentor = await Mentor.findById(req.params.id);
-      oldMentor.show = req.body.show;
-      var updatedMentor = await oldMentor.save();
+            const oldMentor = await Mentor.findById(req.params.id);
+            oldMentor.show = req.body.show;
+            var updatedMentor = await oldMentor.save();
 
 
-      //const updatedMentor = await oldMentor.save();
-      res.json({ success: true });
-    } catch (err) {
-      console.log(err)
-      res
-        .status(404)
-        .json({ success: false, message: err });
+            //const updatedMentor = await oldMentor.save();
+            res.json({ success: true });
+        } catch (err) {
+            console.log(err)
+            res
+                .status(404)
+                .json({ success: false, message: err });
+        }
     }
-  }
 );
 
 
