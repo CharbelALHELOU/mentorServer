dd11c4dfe698050de80dbeee64f2add3b66ed7ea
ging <charbel.alhelou@ensa-paris.fr>
2021-08-26 17:40:58 +0200

    ...

--- routes/user.js
+++ routes/user.js
@@ -24,7 +24,7 @@ function verifyToken(req, res, next) {
 }
 
 
-const sign = '<div class="" style="background-color: #f0f2f5; padding-top: 10px; padding-bottom: 10px; font-family: sans-serif; display: flex;"><div class="" style="display: flex; flex-direction: row; align-items: center; color: #0c3c44; padding-right: 7px;"><img src="https://drive.google.com/uc?export=view&id=1y-Hjshgbjd5I5QivINnijZplQQMdNjxI" alt="" style="height: 60px; width: auto;"></div><div class="" style="color: #0c3c44;"><h3 style="padding: 5px; padding-left: 0; margin: 0;">MentorPack</h3><small style="padding: 0; margin: 0;"><a href="https://www.linkedin.com/company/mentorpack/" style="color: inherit; text-decoration: underline; font-weight: 300;">LinkedIn</a></small></div></div>';
+const sign = '<div class="" style="background-color: #f0f2f5; padding-top: 10px; width:100%;padding-bottom: 10px; font-family: sans-serif; display: flex;"><div class="" style="display: flex; flex-direction: row; align-items: center; color: #0c3c44; padding-right: 7px;"><img src="https://drive.google.com/uc?export=view&id=1y-Hjshgbjd5I5QivINnijZplQQMdNjxI" alt="" style="height: 60px; width: auto;"></div><div class="" style="color: #0c3c44;"><h3 style="padding: 5px; padding-left: 0; margin: 0;">MentorPack</h3><small style="padding: 0; margin: 0;"><a href="https://www.linkedin.com/company/mentorpack/" style="color: inherit; text-decoration: underline; font-weight: 300;">LinkedIn</a></small></div></div>';
 
 /*-------------------------------------------
 formidable = require('formidable'),
