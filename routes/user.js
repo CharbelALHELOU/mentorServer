7544d331227a0de652ff0142347f1d8b2ccb125f
ging <charbel.alhelou@ensa-paris.fr>
2021-08-27 00:11:11 +0200

    ...

--- routes/user.js
+++ routes/user.js
@@ -29,7 +29,7 @@ const sign = '<div class="" style="background-color: #f0f2f5; padding-top: 5px; 
     '<img src="https://drive.google.com/uc?export=view&id=1y-Hjshgbjd5I5QivINnijZplQQMdNjxI" alt="" style="height: 50px; width: auto;">' +
     '</div>' +
     '<div class="" style="color: #0c3c44;padding-left: 5px;">' +
-    '<h3 style="padding: 5px; padding-left: 0; margin: 0;">MentorPack</h3><small style="padding: 0; margin: 0;"><a href="https://www.linkedin.com/company/mentorpack/" style="color: inherit; text-decoration: underline; font-weight: 300;">LinkedIn</a></small></div>' +
+    '<h3 style=" margin: 0;">MentorPack</h3><small style="padding: 0; margin: 0;"><a href="https://www.linkedin.com/company/mentorpack/" style="color: inherit; text-decoration: underline; font-weight: 300;">LinkedIn</a></small></div>' +
     '</div>'
     /*-------------------------------------------
      formidable = require('formidable'),
